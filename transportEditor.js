3665e6c192d3349d782b575ead6404ac6b007ee0
emptyCan <63936804+sousakak@users.noreply.github.com>
2024-03-02 19:34:51 +0900

    +transportEditor

---
+++ transportEditor.js
@@ -0,0 +1,22 @@
+// <source lang="javascript">
+// 
+// 
+/******************************************************************************/
+// ja>>transportEditor
+//   >>交通の記事で視覚的にいくつかの編集を行うツール
+//   >>
+//   >>作者    : Tmv
+//   >>URL    : https://github.com/sousakak/userScriptsForWV/blob/master/transportEditor.js
+//   >>
+//   >>使用方法
+//   >>[[Special:MyPage/common.js]]に以下のコードを追加 : 
+//   >>mw.loader.load('//ja.wikivoyage.org/w/index.php?title=User:Tmv/custom/transportEditor.js&action=raw&ctype=text/javascript');
+/******************************************************************************/
+const i18n = {
+    //
+}
+$(function() {
+    if ($(".voy-stalist").length()) {
+        // 本文
+    }
+});
